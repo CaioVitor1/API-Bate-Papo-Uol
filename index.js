2dbda7e5a141bd273daa716ad73b07ffd0b4f46a
CaioVitor1 <caiovitor33@hotmail.com>
2022-06-24 14:06:49 -0300

    finalizing requirement get - participants

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import joi from 'joi';
+import dayjs from "dayjs"
+
 dotenv.config();
+let time = dayjs().format("HH:mm:ss")
+console.log(time)
 //configurações express
 const app = express();
 app.use(cors());
@@ -13,23 +18,41 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 const promise = mongoClient.connect();
 promise.then(() => db = mongoClient.db("batePapoUol")); //conectando nosso sistema ao banco de dados chamado batePapoUol do Mongo
 
+const participantsSchema = joi.object({
+    name: joi.string().required()
+})
+
+
 app.post('/participants', async (req, res) => {   //falta usar o joy e days aqui
-    const {name} = req.body;
+    const name = req.body;
+
+    const validation = participantsSchema.validate(name, {abortEarly: true})
+    if(validation.error){
+        res.sendStatus(422);
+        return;
+    }
+
     try {
-        if(!name) {
-            return sendStatus(422)
-        }
         await db.collection('participants').insertOne({name: name, lastStatus: Date.now()})
-        await db.collection('message').insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: 'HH:MM:SS'})
+        await db.collection('message').insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: time})
         res.sendStatus(201);
         } catch (error) {
         console.error(error);
         res.sendStatus(500);
         }
     });
+app.get('/participants', async(req, res) => {
+    try {
+        const participants = await db.collection('participants').find().toArray();
+        res.send(participants);
+      } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+      }
+}
+)
 
 
-
-app.listen(5000)
-
-
+app.listen(5000, () => {
+    console.log('API está no ar!');
+  });
